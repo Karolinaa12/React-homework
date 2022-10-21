ed8116d774308582d6150e826654c067ab73e1c9
Karolinaa12 <110235732+Karolinaa12@users.noreply.github.com>
2022-10-21 23:22:57 +0200

    Moved code from sandbox

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import SearchEngine from "./SearchEngine";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <div className="App">
+      <div class="weather-app-wrapper">
+        <div class="weather-app">
+          <SearchEngine />
+        </div>
+      </div>
+    </div>
   </React.StrictMode>
 );
 
